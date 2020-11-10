a268a0cd1d96923dd75b99b7955775d43a98a55d
nohaderf <freda.hon@gmail.com>
2020-11-10 12:47:57 -0500

    added forms and some styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+    def logged_in_user
+        @current_user = User.find_by(id: session[:user_id])
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     private
 
         def user_params
-            params.require(:user).permit(:name, :age, :vip_status)
+            params.require(:user).permit(:first_name, :last_name, :email, :vip_status)
         end
 
 end
